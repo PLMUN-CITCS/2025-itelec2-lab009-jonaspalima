14d223ff41b07005a5c94fbddd0a831d62cc45db
jonaspalima <palimajonas_bsit@plmun.edu.ph>
2025-02-13 20:00:08 +0800

    if_else_statement.py

--- if_else_statement.py
+++ if_else_statement.py
@@ -1,17 +1,11 @@
-user_input = input("Enter a number: ")
-
-number = int(user_input)
-
-if number % 2 == 0:
-  
-    print("The number", number, "is Even.")
-  
-else:
-  
-    print("The number", number, "is Odd.")
-
 try:
-    
+    user_input = input("Enter a number: ")
+    number = int(user_input)
+
+    if number % 2 == 0:
+        print("The number", number, "is Even.")  
+    else:  
+        print("The number", number, "is Odd.")
+   
 except ValueError:
-  
     print("Invalid input. Please enter an integer.")
